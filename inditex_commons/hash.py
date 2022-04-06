7471878c1e4adf44cbf3a6df27562d65fda385ca
damart93 <38332576+damart93@users.noreply.github.com>
2022-04-06 13:48:06 +0200

    Add files via upload

---
+++ inditex_commons/hash.py
@@ -0,0 +1,18 @@
+import hashlib
+import os
+import base64
+from inditex_commons import hash
+
+os.environ['HASH_KEY'] = "test"
+
+
+def hash_string(s):
+    s = s.lower().strip()+os.environ['HASH_KEY']
+    return base64.b64encode(hashlib.sha256((s.lower().strip()+os.environ['HASH_KEY']).encode('utf8')).digest()).decode('utf8')
+
+
+def bulk_hash(data):
+    answer = {}
+    for element in data:
+        answer[element] = hash_string(element)
+    return answer
