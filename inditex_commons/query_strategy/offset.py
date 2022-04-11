ad7911d6b20a263b5bda17884db53dcdcaf81684
damart93 <38332576+damart93@users.noreply.github.com>
2022-04-11 11:49:08 +0200

    Update offset.py

--- inditex_commons/query_strategy/offset.py
+++ inditex_commons/query_strategy/offset.py
@@ -1,73 +1,77 @@
-import hashlib
-import os
-import base64
-import json
-from jsonschema import Draft4Validator, FormatChecker
-from inditex_commons import exceptions
+import math
 
-def hash_string(s):
-    if "HASH_SALT" not in os.environ.keys():
-        raise exceptions.HashNoSalt("Trying to hash without salt. Please set-up 'HASH_SALT' in environment in order to hash data")
+def get_row_count(schema, table, additional_filters=""):
 
-    s = s.lower().strip()+os.environ['HASH_SALT']
-    return base64.b64encode(hashlib.sha256((s.lower().strip()+os.environ['HASH_SALT']).encode('utf8')).digest()).decode('utf8')
+    query = ("SELECT count(1)"
+                " FROM {}.{}").format(schema, table)
+    if additional_filters != "":
+        query += " WHERE {}".format(additional_filters)
+    return query
 
-
-def hash_records(records, fields):
-
-    for record in records:
-        for field in fields:
-            if field in record:
-
-                if record[field] is not None and not isinstance(record[field], str):
-                    try:
-                        value = str(record[field])
-                    except UnicodeDecodeError as ex:
-                        raise ex
-                else:
-                    value = record[field]
-
-                record[field] = hash_string(value)
+def get_queries_by_row_number(pk_columns, desired_columns, escaped_columns, schema, table, db_type, total_count, max_results, additional_filter=""):
+    if db_type == "ORACLE":
+        pk_columns = ",".join(pk_columns)
+        if additional_filter != "":
+            additional_filter = "WHERE " + additional_filter
+        base_query = ("SELECT {}"
+                        " FROM ( SELECT {}, ROWNUM AS RNUM"
+                        " FROM {}.{} T"
+                        f"{additional_filter}"
+                        " ORDER BY {})"
+                        " WHERE RNUM >= {} AND RNUM < {}"
+                        " ORDER BY RNUM")
+        return [base_query.format(','.join(desired_columns), ','.join(escaped_columns), schema, table, pk_columns, i, i + max_results) for i in range(1, total_count, max_results)]
 
 
-def modify_schema_property_type(properties, field):
+def get_queries_by_dense_rank(pk_columns, desired_columns, escaped_columns, schema, table, db_type, total_count, max_results, additional_filter=""):
+    if db_type == "ORACLE":
+        pk_columns = ",".join(pk_columns)
+        if additional_filter != "":
+            additional_filter = " WHERE " + additional_filter
+        base_query = ("SELECT {}"
+                        " FROM ( SELECT {}, DENSE_RANK() OVER (ORDER BY {}) AS RNUM"
+                        " FROM {}.{} T"
+                        f"{additional_filter}"
+                        " ORDER BY {})"
+                        " WHERE RNUM >= {} AND RNUM < {}"
+                        " ORDER BY RNUM")
+        return [base_query.format(','.join(desired_columns), ','.join(escaped_columns), pk_columns, schema, table, pk_columns, i, i + max_results) for i in range(1, total_count, max_results)]
 
-    if field in properties:
-        if "string" in properties[field].type:
-            return
-        else:
-            if "null" in properties[field].type:
-                return ['null', 'string']
-            else:
-                return ['string']
+def get_results_for_queries_number(queries, count):
+    return math.ceil(count/queries)
 
+def adjust_results_for_result_number(results, count):
+    return math.ceil( count / math.ceil(count / results))
 
 def main():
 
-    schema_ex = """{"type": "SCHEMA", "stream": "ONLINE_INFORMACION_ENTREGA", "schema": {"properties": {"ID_ONLINE_TIPO_INFORMACION_ENTREGA": {"inclusion": "available", "minimum": -32768, "maximum": 32767, "type": ["null", "integer"]}, "ID_ONLINE_INFORMACION_ENTREGA": {"inclusion": "automatic", "minimum": -9223372036854775808, "maximum": 9223372036854775807, "type": ["null", "integer"]}, "DESCRIPCION": {"inclusion": "available", "maxLength": 500, "type": ["null", "string"]}, "ID_ONLINE_ENTREGA": {"inclusion": "available", "minimum": -9223372036854775808, "maximum": 9223372036854775807, "type": ["null", "integer"]}}, "type": "object"}, "key_properties": ["ID_ONLINE_INFORMACION_ENTREGA"]}
-    """
-    record_ex = """{"type": "RECORD", "stream": "ONLINE_INFORMACION_ENTREGA", "record": {"ID_ONLINE_TIPO_INFORMACION_ENTREGA": 2, "ID_ONLINE_INFORMACION_ENTREGA": 51511693, "DESCRIPCION": "10", "ID_ONLINE_ENTREGA": 66623709}, "version": 1649322413157}
-    """
+    table = "FACT_ESTIMACION"
+    table_schema = "AAP_DRIVE"
+    partition_column = ["ID_CADENA", "ID_FECHA", "ID_T_PROD", "ID_CAMPANA", "ID_SECCION", "ID_PLAT"]
+    additional_filter = "ID_PLAT = 508"
+    additional_filter = ""
 
-    schema = json.loads(schema_ex)
-    records = [json.loads(record_ex)]
+    max_results = 50000
+    queries = 10
 
-    os.environ['HASH_KEY'] = 'test'
+    total_sql = get_row_count(table_schema, table, additional_filter)
+    print(total_sql)
+    rows = 308014
+    # Either
+    # Number of queries we want to do
+    n_results = get_results_for_queries_number(queries, rows)
+    print(f"Table has {rows} rows, we want {queries} queries done, set max_results:{n_results}")
+    # Or adjust number of results per query
+    n_results = adjust_results_for_result_number(max_results, rows)
+    print(f"Table has {rows} rows, we want at most {max_results} per query, set queries: {math.ceil(rows/n_results)} with max_results:{n_results}")
 
-    fields_to_hash = ['ID_ONLINE_TIPO_INFORMACION_ENTREGA']
+    #mock rows
 
-    modify_schema_properties(schema['schema']['properties'], fields_to_hash)
 
-    with open("file_db2.out") as f:
-        data = [json.loads(line) for line in f.readlines()]
-
-    records = list(filter(lambda x: x['type'] == "RECORD", data))
-
-    hash_records([r['record'] for r in records], fields_to_hash)
-
-    validator = Draft4Validator(schema['schema'], format_checker=FormatChecker())
-    validator.validate(records[0]['record'])
-    validator.validate(json.loads(record_ex)['record'])
+    queries = get_queries_by_row_number(partition_column, table_schema, table, "ORACLE", rows, max_results, additional_filter)
+    for query in queries: print(query)
+    queries = get_queries_by_dense_rank(partition_column, table_schema, table, "ORACLE", rows, max_results, additional_filter)
+    for query in queries: print(query)
 
 if __name__ == '__main__':
     main()
